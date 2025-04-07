58d5ff9acdc4dd55da7934b86aa89bb21e7d6d3d
lizabespalova <bespalovalizv@gmail.com>
2025-04-07 15:15:10 +0300

    changed RegisterController.php

--- app/controllers/auth_controllers/RegisterController.php
+++ app/controllers/auth_controllers/RegisterController.php
@@ -12,7 +12,7 @@ use services\ErrorService;
 
 require_once __DIR__ . '/../../models/User.php';
 require_once __DIR__ .'/../../../config/config.php';
-require_once dirname(__DIR__, 3) . '/views/auth/phpmailer/PHPMailerAutoload.php';
+require_once __DIR__ . '/../../../views/auth/phpmailer/PHPMailerAutoload.php';
 //require_once 'app/services/helpers/session_check.php';
 
 class RegisterController {
