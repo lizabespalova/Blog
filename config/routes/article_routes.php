891938de85b935796e066e060bb172b34594125a
lizabespalova <bespalovalizv@gmail.com>
2025-04-10 23:27:05 +0300

    changed markdown

--- config/routes/article_routes.php
+++ config/routes/article_routes.php
@@ -70,7 +70,7 @@ function article_route($uri, $method) {
             $controller = new ArticleController(getDbConnection());
             $controller->getArticleReactioners($slug); // Передаем slug в метод контроллера
             exit();
-        case $method === 'POST' && preg_match('#^/upload-temp-image$#', $uri):
+        case $method === 'POST' && preg_match('#^/api/upload-temp-image$#', $uri):
             $controller = new ArticleController(getDbConnection());
             $controller->upload_temp_image(); // вызываем нужный метод
             exit();
